834392841734b8babf184ede91db360422a521dc
cmorrow28 <cayleymorrow1@gmail.com>
2023-11-11 10:27:21 +0800

    update files

--- Coding/app.py
+++ Coding/app.py
@@ -9,66 +9,58 @@ app = Flask(__name__)
 
 # Database Setup
 engine = create_engine("sqlite:///Coding/Data_Engineering.db")
-metadata = MetaData()
-metadata.reflect(engine)
-inspector = inspect(engine)
-#print(inspector.get_table_names())
+
 # reflect an existing database into a new model
 Base = automap_base()
 
 # reflect the tables
 Base.prepare(autoload_with=engine)
 
-for cls in Base.classes:
-    print(cls.__name__)
+#Define classes for each database
+EVs = Base.classes.registration
+EV_Chargers = Base.classes.ev_charger
+Fuel_Source = Base.classes.fuel_type
 
-#print(Base.classes.keys())
+# Create an API route for EVs
+@app.route("/api/v1.0/evs")
+def get_EVs():
+    session = Session(engine)
+    results = session.query(EVs).all()
+    session.close()
 
-# #Define classes for each database
-# EVs = Base.classes.registration
-# EV_Chargers = Base.classes.ev_charger
-# Fuel_Source = Base.classes.fuel_type
+    EVs_data = []
+    for result in results:
+        EVs_data.append(result)
+    return jsonify(EVs_data)
 
-# # Create an API route for EVs
-# @app.route("/api/v1.0/evs")
-# def get_EVs():
-#     session = Session(engine)
-#     results = session.query(EVs).all()
-#     session.close()
+# Create an API route for EV Chargers
+@app.route("/api/v1.0/ev_chargers")
+def get_EVs():
+    session = Session(engine)
+    results = session.query(EV_Chargers).all()
+    session.close()
 
-#     EVs_data = []
-#     for result in results:
-#         EVs_data.append(result)
-#     return jsonify(EVs_data)
+    EV_Charger_data = []
+    for result in results:
+        EV_Charger_data.append(result)
+    return jsonify(EV_Charger_data)
 
-# # Create an API route for EV Chargers
-# @app.route("/api/v1.0/ev_chargers")
-# def get_EVs():
-#     session = Session(engine)
-#     results = session.query(EV_Chargers).all()
-#     session.close()
+# # Create an API route for Fuel Sources
+@app.route("/api/v1.0/Fuel_Sources")
+def get_fuel_sources():
+    session = Session(engine)
+    results = session.query(Fuel_Source).all()
+    session.close()
 
-#     EV_Charger_data = []
-#     for result in results:
-#         EV_Charger_data.append(result)
-#     return jsonify(EV_Charger_data)
+    fuel_sources_data = []
+    for result in results:
+        fuel_sources_data.append(result)
+    return jsonify(fuel_sources_data)
 
-# # # Create an API route for Fuel Sources
-# @app.route("/api/v1.0/Fuel_Sources")
-# def get_fuel_sources():
-#     session = Session(engine)
-#     results = session.query(Fuel_Source).all()
-#     session.close()
-
-#     fuel_sources_data = []
-#     for result in results:
-#         fuel_sources_data.append(result)
-#     return jsonify(fuel_sources_data)
-
-# # Create an API route for the Suburbs GeoJSON
-# @app.route("/api/v1.0/NSW_Geo")
-# def get_geojson_data():
-#     # Read GeoJSON data
-#     with open("Resources/suburb-2-nsw.geojson", 'r') as geojson_file:
-#         geojson_data = geojson_file.read()
-#     return jsonify(geojson_data)
+# Create an API route for the Suburbs GeoJSON
+@app.route("/api/v1.0/NSW_Geo")
+def get_geojson_data():
+    # Read GeoJSON data
+    with open("Resources/suburb-2-nsw.geojson", 'r') as geojson_file:
+        geojson_data = geojson_file.read()
+    return jsonify(geojson_data)
